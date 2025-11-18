fb220d979ddbc88a3d5669483c1a8ccc7792a2e4
lpaul <paul.lebouc@ynov.com>
2025-11-18 17:28:16 +0100

    ajout fonctioner clic

--- click/click.go
+++ click/click.go
@@ -5,26 +5,32 @@ import (
     "encoding/json"
 )
 
-var count int = 0
+var count float64 = 0
+var click float64 = 1
+var up float64 = 10
 
 // GetState renvoie l'état actuel du compteur
 func GetState(w http.ResponseWriter, r *http.Request) {
-    json.NewEncoder(w).Encode(map[string]int{"count": count})
+    json.NewEncoder(w).Encode(map[string]float64{"count": count})
 }
-
 // Click incrémente le compteur
 func Click(w http.ResponseWriter, r *http.Request) {
-    count++
-    json.NewEncoder(w).Encode(map[string]int{"count": count})
+    count += click
+    json.NewEncoder(w).Encode(map[string]float64{"count": count})
 }
-
-// Upgrade (exemple simple)
+// Upgrade augmente la valeur du clic
 func Upgrade(w http.ResponseWriter, r *http.Request) {
-    count += 10
-    json.NewEncoder(w).Encode(map[string]int{"count": count})
+    if count >= up {
+    click += 1
+    count -= up
+    up *= 1.5
+    }else {
+    http.Error(w, "Tu ne peux pas améliorer", http.StatusBadRequest)
+    return
+    }
+    json.NewEncoder(w).Encode(map[string]float64{"count": count})
 }
-
 // GetGame renvoie l'état du jeu (exemple)
-func GetGame() map[string]int {
-    return map[string]int{"count": count}
+func GetGame() map[string]float64 {
+    return map[string]float64{"count": count}
 }
